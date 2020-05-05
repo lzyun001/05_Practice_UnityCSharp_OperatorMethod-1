7a218c571ee5e0886e28f9836b19ee85d351e3a5
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-05-05 14:27:38 +0800

    聯繫完成

---
+++ 05_Practice_UnityCSharp_OperatorMethod-1/Assets/Slime.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slime : MonoBehaviour
+{
+    [Header("血量")]
+    public int hp = 200;
+    [Header("攻擊力")]
+    public int att = 20;
+    [Header("治愈量")]
+    public int heal = 10;
+    public Text result;
+    public Bat bat;
+
+    public void Attack()
+    {
+        bat.Injured();
+    }
+
+    public void Injured()
+    {
+        hp -= bat.att;
+        result.text = "<color=#9CA610>" + gameObject.name + "</color> - 受到傷害：" + bat.att + "\n <color=#9CA610>" + gameObject.name + "</color> - 血量剩下：" + hp;
+    }
+
+    public void Heal()
+    {
+        hp += heal;
+        result.text = "<color=#9CA610>" + gameObject.name + "</color> - 收到治愈：" + heal + "\n <color=#9CA610>" + gameObject.name + "</color> - 血量剩下：" + hp;
+    }
+
+
+}
